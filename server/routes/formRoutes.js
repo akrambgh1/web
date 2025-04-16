fb4a2c8042846578d4d9710f2530763571d673da
akrambgh1 <121614726+akrambgh1@users.noreply.github.com>
2025-04-16 05:03:53 +0100

    Update formRoutes.js

--- server/routes/formRoutes.js
+++ server/routes/formRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { submitForm,getAllForms, updateFormStatus } = require('../controllers/formController');
 const cors = require('cors');
 router.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'web-pi-pied.vercel.app',
     credentials: true
   }));
   
