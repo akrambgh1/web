10313d5ad5c8d015134ce40b1d2b383b658acbaf
akrambgh1 <121614726+akrambgh1@users.noreply.github.com>
2025-04-16 05:49:55 +0100

    Update App.jsx

--- web-v2/src/App.jsx
+++ web-v2/src/App.jsx
@@ -6,7 +6,7 @@ import Contact from "../pages/Contact";
 import axios from "axios";
 
 import Admin from "../pages/admin";
-axios.defaults.baseURL ="https://weblocators-backend.onrender.com/";
+axios.defaults.baseURL =import.meta.env.VITE_API_URL;
 // Ensure the environment variable is properly accessed
 export default function App() {
   return (
