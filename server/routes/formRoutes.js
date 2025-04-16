0b11fa623231daa6deb32c4238f143e8f1b0a10c
akrambgh1 <121614726+akrambgh1@users.noreply.github.com>
2025-04-16 18:28:30 +0100

    a

--- server/routes/formRoutes.js
+++ server/routes/formRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { submitForm,getAllForms, updateFormStatus } = require('../controllers/formController');
 const cors = require('cors');
 router.use(cors({
-    origin: 'https://web-pi-pied.vercel.app' 
+    origin: [ 'https://web-pi-pied.vercel.app' ,"https://web-taw6.vercel.app"],
     credentials: true
   }));
   
