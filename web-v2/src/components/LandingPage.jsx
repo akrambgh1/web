3abe1644c1a8e2391a71ca36d14638c2b104f625
akrambgh1 <121614726+akrambgh1@users.noreply.github.com>
2025-07-30 14:10:10 +0100

    a

--- web-v2/src/components/LandingPage.jsx
+++ web-v2/src/components/LandingPage.jsx
@@ -3,13 +3,17 @@ export default function LandingPage() {
   return (
     <>
     <div className="h-[100dvh] p-8">
-      <div className="w-full relative h-full [background:_radial-gradient(circle_at_10%_20%,_#2264f1_0%,_rgb(7,_91,_173)_90%);] rounded-[25px] flex flex-col items-center justify-center">
-        
-        <div className="absolute bottom-8 right-8 bg-white h-[30rem] w-[50rem] rounded-[25px] p-8 flex flex-col gap-4">
+      <div className="grid  grid-cols-5 grid-rows-6 gap-0  w-full relative h-full rounded-[25px] flex-col items-center justify-center">
+        <div  className=" col-span-2 row-span-3 z-0 col-start-4 row-start-4  h-full rounded-bl-4xl rounded-tr-4xl rounded-br-4xl bg-[#2264f1] p-8 flex flex-col gap-4"></div>
+        <div  className=" col-span-2  z-40 row-span-3  col-start-4 row-start-4  bg-white h-full  rounded-2xl p-8 flex flex-col gap-4">
         <h1 className="text-[3rem] font-[500] tracking-tighter w-[90%]">Transforming ideas into impactful digital experiences.</h1>
         <p className="text-[#9ea2ae] text-[1.5rem] w-[80%] tracking-tighter">We bring ideas to life with elegant design and smart technology, creating digital experiences that captivate and deliver real impact.</p>
         <button className="bg-[#1e1e1e] text-white mt-[2rem] w-fit px-8 py-2 rounded-[7px] cursor-pointer">Contact Us</button>
-        </div>
+          </div>
+          <div className="col-span-3 row-span-3 rounded-tl-2xl h-full bg-[#2264f1] "></div>
+          <div className="col-span-2 row-span-3 h-full col-start-4 rounded-br-2xl rounded-tr-2xl bg-[#2264f1]"></div>
+     
+    <div className="col-span-3 row-span-3 col-start-1 rounded-bl-2xl rounded-br-2xl  row-start-4 bg-[#2264f1] h-full"></div>
       </div>
     </div>
     </>
