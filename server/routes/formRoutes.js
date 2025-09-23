6f072864324cb3a037c1adeacce7f6d598bb486b
akrambgh <akrambgh316@gmail.com>
2025-09-23 21:02:04 +0100

    a

--- server/routes/formRoutes.js
+++ server/routes/formRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { submitForm, getAllForms, updateFormStatus } = require('../controllers/formController');
-const { newsletter ,getNewsLetter } =require('../controllers/newsletterController')
+const { addNewsletterEmail , getNewsletterEmails } =require('../controllers/newsletterController')
 const cors = require('cors');
 router.use(cors({
     origin: [ 'https://web-pi-pied.vercel.app' ,"https://web-taw6.vercel.app"],
@@ -12,9 +12,9 @@ router.use(cors({
 router.post('/', submitForm);
 router.get('/admin', getAllForms);
 
-router.get('/admin/emails', getNewsLetter);
+router.get('/admin/emails',  getNewsletterEmails);
 router.put('/status', updateFormStatus);
-router.post('/emails', newsletter);
+router.post('/emails', addNewsletterEmail);
 
   
 
